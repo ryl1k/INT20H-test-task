f1aee5fa9787d6402e02a489c9c14fde7c7e9f3e
ryl1k <ruslan4ukshev4yk@gmail.com>
2026-02-28 15:13:24 +0200

    fixed merge conflicts

--- server/internal/controller/http/router.go
+++ server/internal/controller/http/router.go
@@ -44,27 +44,6 @@ func NewRouter(
 }
 
 func (r *Router) RegisterRoutes() {
-<<<<<<< HEAD
-	swaggerHandler := echoSwagger.EchoWrapHandler()
-	r.echo.GET("/swagger/*", func(c echo.Context) error {
-		// Keep Swagger endpoint host/scheme aligned with the current request,
-		// so "Try it out" works in both local Docker and deployed environments.
-		if host := c.Request().Host; host != "" {
-			docs.SwaggerInfo.Host = host
-		}
-
-		scheme := c.Scheme()
-		if scheme == "" {
-			scheme = "http"
-		}
-		docs.SwaggerInfo.Schemes = []string{scheme}
-		docs.SwaggerInfo.BasePath = "/"
-
-		return swaggerHandler(c)
-	})
-
-=======
->>>>>>> 1e54adcf8ac827f7beaecc3a6e668d2c29a0ca92
 	r.echo.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
 		AllowHeaders: []string{
